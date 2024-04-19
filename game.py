c15364d6bd9a2429ebd1105bb9ad950f07ffacac
STELLAHERZBERG <SHERZBE@CLEMSON.EDU>
2024-04-19 14:55:02 -0400

    description

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print('hello')
+'''
+Name: Stella Herzberg
+Date: 04/18/2024
+Class: CPSC 1050
+Section: 001
+Assignment: Project 2 RPG
+
+Description: This creates a RPG where the user interacts as a character within a cave. The user will attempt
+to move through various rooms, each one containing either a treasure chest with supplies or a monster that they
+must fight. The user wins if they successfully navigate through all of the rooms without dying.
+'''
+
